8d0f4495a2b312c59ac603afcd162640704f4cc1
박유경 <pyk710_@naver.com>
2026-04-11 01:20:41 +0900

    feat: 키오스크 구현

--- kio-frontend/src/assets/index.ts
+++ kio-frontend/src/assets/index.ts
@@ -1,2 +1,5 @@
 export { default as logo } from "./icons/KIO_logo.svg";
 export { default as coffee1 } from "./images/coffee1.svg";
+export { default as imgCardPayment } from "./images/카드결제.svg";
+export { default as imgAppCard } from "./images/앱카드결제.svg";
+export { default as imgBarcode } from "./images/바코드.svg";
