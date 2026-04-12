e3b96b474ec87032bed833cc466c41fe7d6c1464
박유경 <pyk710_@naver.com>
2026-04-12 23:17:35 +0900

    feat: 메뉴사진 정리

--- kio-frontend/src/assets/index.ts
+++ kio-frontend/src/assets/index.ts
@@ -1,24 +1,24 @@
 export { default as logo } from "./icons/KIO_logo.svg";
 
-export { default as coffee1 } from "./images/coffee1.svg";
-export { default as imgCafeLatte } from "./images/카페라떼.svg";
-export { default as imgCappuccino } from "./images/카푸치노.svg";
-export { default as imgVanillaLatte } from "./images/바닐라라떼.svg";
-export { default as imgCaramelMacchiato } from "./images/카라멜마끼아토.svg";
-export { default as imgEspresso } from "./images/에스프레소.svg";
-export { default as imgStrawberrySmoothie } from "./images/딸기스무디.svg";
-export { default as imgMangoSmoothie } from "./images/망고스무디.svg";
-export { default as imgBlueberrySmoothie } from "./images/블루베리스무디.svg";
-export { default as imgLemonAde } from "./images/레몬에이드.svg";
-export { default as imgGreengrapesAde } from "./images/청포도에이드.svg";
-export { default as imgGapefruitAde } from "./images/자몽에이드.svg";
-export { default as imgOrangejuice } from "./images/오렌지주스.svg";
-export { default as imgCarrotjuice } from "./images/당근주스.svg";
-export { default as imgApplejuice } from "./images/사과주스.svg";
-export { default as imgHibiscus } from "./images/히비스커스.svg";
-export { default as imgEarlgrey } from "./images/얼그레이.svg";
-export { default as imgPeppermint } from "./images/페퍼민트.svg";
-export { default as imgChamomile } from "./images/캐모마일.svg";
+export { default as coffee1 } from "./images/음료사진/coffee1.svg";
+export { default as imgCafeLatte } from "./images/음료사진/카페라떼.svg";
+export { default as imgCappuccino } from "./images/음료사진/카푸치노.svg";
+export { default as imgVanillaLatte } from "./images/음료사진/바닐라라떼.svg";
+export { default as imgCaramelMacchiato } from "./images/음료사진/카라멜마끼아토.svg";
+export { default as imgEspresso } from "./images/음료사진/에스프레소.svg";
+export { default as imgStrawberrySmoothie } from "./images/음료사진/딸기스무디.svg";
+export { default as imgMangoSmoothie } from "./images/음료사진/망고스무디.svg";
+export { default as imgBlueberrySmoothie } from "./images/음료사진/블루베리스무디.svg";
+export { default as imgLemonAde } from "./images/음료사진/레몬에이드.svg";
+export { default as imgGreengrapesAde } from "./images/음료사진/청포도에이드.svg";
+export { default as imgGapefruitAde } from "./images/음료사진/자몽에이드.svg";
+export { default as imgOrangejuice } from "./images/음료사진/오렌지주스.svg";
+export { default as imgCarrotjuice } from "./images/음료사진/당근주스.svg";
+export { default as imgApplejuice } from "./images/음료사진/사과주스.svg";
+export { default as imgHibiscus } from "./images/음료사진/히비스커스.svg";
+export { default as imgEarlgrey } from "./images/음료사진/얼그레이.svg";
+export { default as imgPeppermint } from "./images/음료사진/페퍼민트.svg";
+export { default as imgChamomile } from "./images/음료사진/캐모마일.svg";
 
 
 export { default as imgCardPayment } from "./images/결제사진/카드결제.svg";
