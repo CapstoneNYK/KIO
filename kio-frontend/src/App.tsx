f302e0ae43d6bef44f161048b4d015331638f090
박유경 <pyk710_@naver.com>
2026-05-18 21:30:31 +0900

    feat: ocr DB 구현

--- kio-frontend/src/App.tsx
+++ kio-frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { logo } from "./assets";
+import { AssistantButton } from "./components/AssistantButton";
 
 export default function App() {
   return (
     <div className="min-h-screen w-full bg-[#FFEFCE]">
-      <img
-        src={logo}
-        alt="KIO"
-        className="fixed top-4 right-4 w-20 h-20 rounded-full z-9999 pointer-events-none"
-      />
+      <AssistantButton />
       <div className="w-full min-h-screen flex flex-col">
         <Outlet />
       </div>
